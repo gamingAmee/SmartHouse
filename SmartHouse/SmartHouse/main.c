cb0330f0b3f05bd53a14067dfd89fd550368c67c
gamingAmee <amalie2410@gmail.com>
2020-12-03 12:15:42 +0100

    Projects added

---
+++ SmartHouse/SmartHouse/main.c
@@ -0,0 +1,18 @@
+/*
+ * SmartHouse.c
+ *
+ * Created: 03-12-2020 09:35:17
+ * Author : Patrick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ SmartHouse/SmartHouselib/library.c
@@ -0,0 +1,16 @@
+/*
+ * SmartHouselib.c
+ *
+ * Created: 03-12-2020 12:15:08
+ * Author : Patrick
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
